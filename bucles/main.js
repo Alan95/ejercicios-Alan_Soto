d5419b39914b93968b45eadab5eef48135532359
Alan Soto <alan95soto@gmail.com>
2024-07-17 14:44:25 -0300

    beginning of loop exercises

---
+++ bucles/main.js
@@ -0,0 +1,20 @@
+// /* Ejercicio 1 */
+
+// let numero = parseInt(prompt("Ingresa un numero"))
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`${numero} x ${i} = ${numero * i}`)
+// }
+
+// /* Ejercicio 2 */
+
+// let acumulador = 0
+
+// do {
+//     numero = parseInt(prompt("Ingresa un numero"))
+//     acumulador += numero
+//     console.log(acumulador)
+// } while (numero !== 0)
+
+/* Ejercicio 3 */
+
